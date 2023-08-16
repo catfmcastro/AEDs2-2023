3c9a4e71fee42b3f08f92c1954e588fff82fc426
Catarina França De Moura Castro <1445017@sga.pucminas.br>
2023-08-16 12:14:43 -0300

    update ativdades lab

---
+++ lab/lab-16-28/Prog.java
@@ -0,0 +1,83 @@
+import java.util.*;
+import java.io.*;
+
+/*
+ * Aula de laboratorio - 16/08/23
+ * 
+ * Catarina F. M. Castro - 803531
+ *
+ * AEDs II
+ * */
+
+// testando e brincando com classes e herancas
+
+class Person{
+	// Declaracao de atributos privados
+	protected String name;
+	protected int age;
+
+	// metodo construtivo
+	public Person(String name, int age){
+		this.name = name;
+		this.age = age;
+
+		setAge(age);
+
+	}
+
+	// metodos getter e setter
+	public String getName(){
+		return this.name;
+	}
+
+	public int getAge(){
+		return this.age;
+	}
+
+	public void setAge(int age){
+		if(hasValidAge(age)){
+			this.age = age;
+		}
+
+	}
+
+	// checa se o cadastro de idade 
+	public boolean hasValidAge(int age){
+		if(age > 0){
+			return true;
+		}
+
+		return false;
+	}
+}
+
+class Student extends Person{
+	private int id;
+
+	// construtor
+	public Student(String name, int age, int id){
+		// metodo que chama o construtor da classe pai
+		super(name, age);
+		
+		this.id = id;
+	
+	}
+
+	// getter e setter
+	public int getId(){
+		return id;
+	}
+
+
+}
+
+class Prog{
+	public static void main(String[] args){
+		//declaracao de variaveis
+		Person p = new Person("Cat", 18);
+
+		// imprime os resultados na tela
+		System.out.println("Hello, my name is " + p.name + " and I am " + p.age + " years old.");
+	}
+}
+
