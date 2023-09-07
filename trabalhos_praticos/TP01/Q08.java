e2653b24a5b7533a476cb78a0ec093efcb6aea55
Catarina F. M. Castro <catfmcastro@gmail.com>
2023-09-06 21:03:06 -0300

    fim q08

--- trabalhos_praticos/TP01/Q08.java
+++ trabalhos_praticos/TP01/Q08.java
@@ -10,47 +10,54 @@
 // Ler um inteiro n que indica o número de valores reais que serão lidos e salvos em um arquivo texto. Fechar o arquivo
 // Reabrir o arquivo e utilizar os métodos getFilePointer e seek (da classe RAF) e mostrar os valores lidos na tela
 
-package TP01;
+//package TP01;
 
-import java.io.*;
+import java.io.RandomAccessFile;
 
 public class Q08 {
     public static void main(String[] args) throws Exception{
-        
-        // Declaraçao de variaveis
-        RandomAccessFile raf = new RandomAccessFile ("arq.txt","rw");
         int n = MyIO.readInt(); // quant de numeros que serão inseridos
-
-        // entradas
-        for(int i = 0; i<n; i++){
-            double numero = MyIO.readDouble();
-
-            raf.writeDouble(numero);
-            raf.writeChar('\n');
-        }
-
-        // Fechamento de arquivo
-        raf.close();
-
-        // Reabre o arquivo
-        raf = new RandomAccessFile("arq.txt", "r");
-
-        // percorre o arquivo de modo invertido
-        for(int i = 0; i < n; i++){
-            raf.seek((n-i-1) * 8); // última posição do arquivo * 8 (tamanho de um double)
-
-            double num = raf.readDouble(); // salva o número da linha em um double
-            int aux = (int) num; // converte para inteiro
-
-            if(num - aux == 0){
-                MyIO.println(aux);
+            try{
+                // Declaraçao de variaveis
+                RandomAccessFile raf = new RandomAccessFile ("arq.txt","rw");
+        
+                // entradas
+                for(int i = 0; i<n; i++){
+                    // double numero = MyIO.readDouble();
+                    raf.writeDouble(MyIO.readDouble());
+                    raf.writeBytes("\n");
+                }
+        
+                // Fechamento de arquivo
+                raf.close();
+        
+                // Reabre o arquivo
+                raf = new RandomAccessFile("arq.txt", "r");
+    
+                // Posiciona o ponteiro no final do arq
+                raf.seek(raf.length());
+        
+                // percorre o arquivo
+                for(int i = 0; i < n; i++){
+                    raf.seek((n - i - 1) * (Double.BYTES + 1)); // Calcula a posição correta
+                    double num = raf.readDouble(); // lê o número do arquivo
+                    int aux = (int) num; 
+                    
+                    // imprime o resultado na tela
+                    if(num - aux == 0){
+                        MyIO.println(aux);
+                    }else{
+                        MyIO.println(num);
+                    }
+          
+                }
+        
+                // fechamento do arquivo
+                raf.close();
             }
-            else{
-                MyIO.println(num);
+            catch(Exception e){
+                System.out.println(e.getMessage());
             }
-        }
 
-        // fechamento do arquivo
-        raf.close();
     }
 }
